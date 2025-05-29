a6f58d9d141b4c0a330e31f7688fa00309103d17
MJY1993 <mjylove26@gmail.com>
2025-05-29 15:09:34 +0900

    initialized a git repo

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+def index(request):
+
+    data = {"msg": "서버가 작동 중입니다."}
+
+    return JsonResponse(
+        data, safe=False, json_dumps_params={"ensure_ascii": False}, status=200
+    )

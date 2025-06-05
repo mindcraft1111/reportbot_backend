c5b7b28fedf83fad956e9aeb55460fc2d2ff2468
uujinu <ud071233@gmail.com>
2025-06-05 16:52:29 +0900

    feat: cors 및 DB연결 설정

--- reportbot/settings.py
+++ reportbot/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "api",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +57,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:11235", "http://116.125.140.113:11235"]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "reportbot.urls"
 
@@ -81,8 +86,12 @@ WSGI_APPLICATION = "reportbot.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mindcraft_db",
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("SERVER_HOST"),
+        "PORT": 3306,
     }
 }
 

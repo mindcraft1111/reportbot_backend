25869d23513469b4787753591ce4ed3376d1c00d
youjin Cho <ud071233@gmail.com>
2025-06-11 01:45:27 +0900

    feat: SoftDeleteManager 설정

--- core/models/soft_delete.py
+++ core/models/soft_delete.py
@@ -3,9 +3,22 @@ from django.db import models
 from django.db.models.query import QuerySet
 
 
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQueryset(self.model, using=self._db).alive()
+
+    def all_with_deleted(self):
+        return SoftDeleteQueryset(self.model, using=self._db)
+
+    def only_deleted(self):
+        return SoftDeleteQueryset(self.model, using=self._db).deleted()
+
+
 class SoftDeleteMixin(models.Model):
     is_deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeleteManager()
 
     def delete(self, using=None, keep_parents=False):
         self.is_deleted = True
@@ -19,23 +32,12 @@ class SoftDeleteMixin(models.Model):
 class SoftDeleteQueryset(QuerySet):
     def delete(self):
         return super().update(is_deleted=True, deleted_at=timezone.now())
-    
+
     def hard_delete(self):
         return super().delete()
-    
+
     def alive(self):
         return self.filter(is_deleted=False)
-    
+
     def deleted(self):
         return self.filter(is_deleted=True)
-
-
-class SoftDeleteManager(models.Manager):
-    def get_queryset(self):
-        return SoftDeleteQueryset(self.model, using=self._db).alive()
-    
-    def all_with_deleted(self):
-        return SoftDeleteQueryset(self.model, using=self._db)
-    
-    def only_deleted(self):
-        return SoftDeleteQueryset(self.model, using=self._db).deleted()

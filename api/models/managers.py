f32b5c123f5316553eacae3ed8158f7672bbae33
uujinu <ud071233@gmail.com>
2025-06-12 17:22:11 +0900

    fix: superuser 생성 시 join_date 초기화

--- api/models/managers.py
+++ api/models/managers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
@@ -13,7 +14,8 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
+        extra_fields.setdefault('join_date', timezone.now())
+        
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
